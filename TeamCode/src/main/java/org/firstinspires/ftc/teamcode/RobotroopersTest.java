d3c2a54a2e9052f67b2b5d9a28702ddaac3104c6
thomas gan <thomas.c.gan@gmail.com>
2021-10-17 18:53:49 -0400

    initinitial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotroopersTest.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class RobotroopersTest extends OpMode {
+
+    DcMotor thing;
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
