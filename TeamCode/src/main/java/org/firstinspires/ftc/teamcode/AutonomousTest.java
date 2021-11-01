1cd28c08aab61fdbabd3fb7c7177311d675df0df
TheCoolGuy123 <Ritwik>
2021-11-01 14:13:16 -0400

    Joystick and Autonomous test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous (name= "AutonomousTest")
+public class AutonomousTest extends LinearOpMode {
+
+    DcMotor FR;
+    DcMotor FL;
+    DcMotor BR;
+    DcMotor BL;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        FR = hardwareMap.dcMotor.get("FR");
+        FL = hardwareMap.dcMotor.get("FL");
+        BR = hardwareMap.dcMotor.get("BR");
+        BL = hardwareMap.dcMotor.get("BL");
+
+        FR.setDirection(DcMotor.Direction.REVERSE);
+        FL.setDirection(DcMotor.Direction.FORWARD);
+        FR.setDirection(DcMotor.Direction.FORWARD);
+        FL.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        FL.setPower(1);
+        FR.setPower(1);
+        BL.setPower(1);
+        BR.setPower(1);
+
+        sleep(2000);
+
+        FL.setPower(0);
+        FR.setPower(0);
+        BL.setPower(0);
+        BR.setPower(0);
+
+        FL.setPower(-.5);
+        FR.setPower(.5);
+        BL.setPower(0);
+        BR.setPower(0);
+
+        sleep(4000);
+
+        FL.setPower(0);
+        FR.setPower(0);
+        BL.setPower(0);
+        BR.setPower(0);
+
+        FL.setPower(1);
+        FR.setPower(1);
+        BL.setPower(1);
+        BR.setPower(1);
+
+        sleep(2600);
+
+        FL.setPower(0);
+        FR.setPower(0);
+        BL.setPower(0);
+        BR.setPower(0);
+
+
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControllerTest.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import java.util.Arrays;
+
+@TeleOp
+public class ControllerTest extends OpMode {
+
+    DcMotor FL, FR, BL, BR;
+
+    float FLPower;
+    float FRPower;
+    float BLPower;
+    float BRPower;
+
+    @Override
+    public void init() {
+        FL = hardwareMap.get(DcMotor.class, "FL");
+        FR = hardwareMap.get(DcMotor.class, "FR");
+        BL = hardwareMap.get(DcMotor.class, "BL");
+        BR = hardwareMap.get(DcMotor.class, "BR");
+
+        FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        FR.setDirection(DcMotor.Direction.REVERSE);
+        FL.setDirection(DcMotor.Direction.FORWARD);
+        BR.setDirection(DcMotor.Direction.FORWARD);
+        BL.setDirection(DcMotor.Direction.REVERSE);
+
+    }
+
+    @Override
+    public void loop() {
+
+        FLPower  = (gamepad1.left_stick_y + -gamepad1.right_stick_x);
+        FRPower  = (gamepad1.left_stick_y + gamepad1.right_stick_x);
+        BLPower  = (gamepad1.left_stick_y + -gamepad1.right_stick_x);
+        BRPower  = (gamepad1.left_stick_y + gamepad1.right_stick_x);
+
+        FL.setPower(FLPower);
+        FR.setPower(FRPower);
+        BL.setPower(BLPower);
+        BR.setPower(BRPower);
+
+
+
+    }
+
+
+
+
+
+
+
+//        for (DcMotor dcMotor : Arrays.asList(FL, FR, BL, BR)) {
+//            dcMotor.setPower(gamepad1.left_trigger);
+//        }
+//
+//            FRPower = (gamepad1.right_stick_x);
+//            BRPower = (-gamepad1.right_stick_x);
+//
+//            FR.setPower(FRPower);
+//            BR.setPower(BRPower);
+
+
+}
+
+
+
