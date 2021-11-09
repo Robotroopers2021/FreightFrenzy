a6651d01d998da0cee7fdd41b951f3b5b7728ac8
TheCoolGuy123 <Ritwik>
2021-11-08 20:12:18 -0500

    Servos and Motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControllerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControllerTest.java
@@ -3,17 +3,18 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServoImpl;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import java.util.Arrays;
 
 @TeleOp
 public class ControllerTest extends OpMode {
 
+    //Identifying Motors and Servos
     DcMotor FL, FR, BL, BR;
-    CRServoImpl LH;
-
+    Servo LH;
+    //Create Motor Power
     float FLPower;
     float FRPower;
     float BLPower;
@@ -22,18 +23,19 @@ public class ControllerTest extends OpMode {
 
     @Override
     public void init() {
+        //Connect Motor
         FL = hardwareMap.get(DcMotor.class, "FL");
         FR = hardwareMap.get(DcMotor.class, "FR");
         BL = hardwareMap.get(DcMotor.class, "BL");
         BR = hardwareMap.get(DcMotor.class, "BR");
-
-        LH = (CRServoImpl) hardwareMap.get(CRServoImpl.class, "LH");
-
+        //Connect Servo
+        LH = (Servo) hardwareMap.get(Servo.class, "LH");
+        //Set ZERO POWER BEHAVIOR
         FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
+        //Set Up Motor Direction
         FR.setDirection(DcMotor.Direction.REVERSE);
         FL.setDirection(DcMotor.Direction.FORWARD);
         BR.setDirection(DcMotor.Direction.FORWARD);
@@ -44,20 +46,43 @@ public class ControllerTest extends OpMode {
 
     @Override
     public void loop() {
-
+        //Link Motors to Joysticks
         FLPower = (gamepad1.left_stick_y + -gamepad1.right_stick_x);
         FRPower = (gamepad1.left_stick_y + gamepad1.right_stick_x);
         BLPower = (gamepad1.left_stick_y + -gamepad1.right_stick_x);
         BRPower = (gamepad1.left_stick_y + gamepad1.right_stick_x);
-
+        //Set Motor Power
         FL.setPower(FLPower);
         FR.setPower(FRPower);
         BL.setPower(BLPower);
         BR.setPower(BRPower);
+        //Link Servo to Controller Buttons
+        if (gamepad1.y) {
+            // move to 0 degrees.
+            LH.setPosition(0);
+        } else if (gamepad1.x) {
+            // move to 90 degrees.
+            LH.setPosition(0.5);
+        } else if (gamepad1.b) {
+            //move to 90 degrees opposite direction
+            LH.setPosition(-0.5);
+        }else if (gamepad1.a) {
+            // move to 180 degrees.
+            LH.setPosition(1);
+        }
+        //Adding Telemetry
+        telemetry.addData("Servo Position", LH.getPosition());
+        telemetry.addData("Motor Power", FL.getPower());
+        telemetry.addData("Motor Power", FR.getPower());
+        telemetry.addData("Motor Power", BL.getPower());
+        telemetry.addData("Motor Power", BR.getPower());
+        telemetry.addData("Status", "Running");
+        telemetry.update();
 
-        LH.setPower(gamepad1.right_stick_y);
     }
 }
+
+
 
 
 
@@ -82,12 +107,7 @@ public class ControllerTest extends OpMode {
 //        for (DcMotor dcMotor : Arrays.asList(FL, FR, BL, BR)) {
 //            dcMotor.setPower(gamepad1.left_trigger);
 //        }
-//
-//            FRPower = (gamepad1.right_stick_x);
-//            BRPower = (-gamepad1.right_stick_x);
-//
-//            FR.setPower(FRPower);
-//            BR.setPower(BRPower);
+
 
 
 
