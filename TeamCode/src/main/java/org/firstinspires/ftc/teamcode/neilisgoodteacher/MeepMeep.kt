70e336f7298cd85c99e9368d7f0a16772b6be8dd
salban <salban.nithilaselvan@gmail.com>
2022-06-15 15:19:17 -0400

    🦊 swiper keep swiping

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neilisgoodteacher/MeepMeep.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/neilisgoodteacher/MeepMeep.kt
@@ -5,7 +5,9 @@ import com.acmerobotics.roadrunner.geometry.Vector2d
 import com.noahbres.meepmeep.MeepMeep
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder
 import com.noahbres.meepmeep.roadrunner.DriveShim
-
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive
+import org.firstinspires.ftc.teamcode.drive.DriveConstants
+import org.firstinspires.ftc.teamcode.util.math.Pose
 
 object MeepMeep {
     @JvmStatic
@@ -15,12 +17,37 @@ object MeepMeep {
             .setConstraints(60.0, 40.0, Math.toRadians(80.0), Math.toRadians(30.0), 13.25    )
             .setDimensions(12.5, 18.0)
             .followTrajectorySequence { drive: DriveShim ->
-                drive.trajectorySequenceBuilder(Pose2d(6.25, 61.5, Math.toRadians(90.0)))
+                drive.trajectorySequenceBuilder(Pose2d(6.25, 63.0, Math.toRadians(90.0)))
                     .setReversed(true)
-                    .splineToSplineHeading(Pose2d(1.0, 34.0, Math.toRadians(40.0)), Math.toRadians(240.0))
+
+                    .lineToLinearHeading( Pose2d(-5.0, 47.0, -Math.toRadians(295.0)),
+                    SampleMecanumDrive.getVelocityConstraint(35.0, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                    SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                    .setReversed(false)
+                    .splineToSplineHeading( Pose2d(17.0, 64.0, Math.toRadians(360.0)), -Math.toRadians(360.0))
+                    .splineToConstantHeading( Vector2d(42.0, 64.0), Math.toRadians(0.0),
+                        SampleMecanumDrive.getVelocityConstraint(40.0, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(20.0))
+                    .setReversed(true)
+                    .splineToConstantHeading( Vector2d(17.0, 64.0), -Math.toRadians(180.0),
+                        SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                    .splineTo( Vector2d(-5.0, 44.2), Math.toRadians(260.0),
+                    SampleMecanumDrive.getVelocityConstraint(55.0, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                    SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+//                    .splineToSplineHeading(Pose2d(-5.0, 47.0, -Math.toRadians(295.0)), Math.toRadians(265.5))
+//                    .lineToLinearHeading(Pose2d(1.0, 42.0, Math.toRadians(60.0)))
+//                    .setReversed(false)
+//                    .splineToSplineHeading(Pose2d(12.0, 64.0, Math.toRadians(0.0)), Math.toRadians(40.0))
+//                    .lineToLinearHeading(
+//                        Pose2d(40.0, 64.0, Math.toRadians(0.0)),
+//                        SampleMecanumDrive.getVelocityConstraint(40.0, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+//                        SampleMecanumDrive.getAccelerationConstraint(20.0))
+//                    .setReversed(true)
+//                    .splineToSplineHeading(Pose2d(12.0, 64.0, Math.toRadians(0.0)), Math.toRadians(0.0))
                     .build()
             }
-        meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_OFFICIAL)
             .setDarkMode(true)
             .setBackgroundAlpha(0.95f)
             .addEntity(myBot)
