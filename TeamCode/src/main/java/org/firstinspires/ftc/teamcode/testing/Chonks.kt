299ea1ce06946b1d904fb802e44b16fb1e0388dc
ricks <ricksinha08@gmail.com>
2022-08-31 20:06:11 -0400

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Chonks.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Chonks.kt
@@ -13,16 +13,56 @@ open class Chonks : OpMode() {
     lateinit var fr:DcMotor
     lateinit var bl:DcMotor
     lateinit var br:DcMotor
+    lateinit var arm:DcMotor
+    lateinit var duckSpinner:DcMotor
+    lateinit var hi:DcMotor
 
     var drive = 0.0
     var strafe = 0.0
     var rotate = 0.0
+    var armpower = 0.4
+    var  duck = 0.4
+    var hilights= 0.0
+
 
     private fun driveControl() {
         drive = (-gamepad1.left_stick_y.toDouble()) * 0.85
-        strafe= (-gamepad1.left_stick_x.toDouble()) * 0.85
-        rotate= (-gamepad1.right_stick_x.toDouble()) * 0.85
+        strafe= (gamepad1.left_stick_x.toDouble()) * 0.85
+        rotate= (gamepad1.right_stick_x.toDouble()) * 0.85
+
+        fl.power = drive + strafe + rotate
+        fr.power = drive - strafe - rotate
+        bl.power = drive - strafe + rotate
+        br.power = drive + strafe - rotate
     }
+
+   private fun armControl() {
+        if (gamepad1.dpad_left) {
+            arm.power = armpower
+        } else if(gamepad1.dpad_right) {
+            arm.power = -armpower
+        } else {
+           arm.power=0.0
+        }
+   }
+
+    private fun duckControl() {
+
+        if (gamepad1.dpad_up) {
+            duckSpinner.power = duck
+        } else if (gamepad1.dpad_down) {
+            duckSpinner.power = -duck
+        }else {
+            duckSpinner.power=0.0
+        }
+    }
+    private fun hiControl() {
+        hilights= (gamepad2.right_stick_x.toDouble()) * 0.85
+        hi.power=hilights
+
+
+        }
+
 
     override fun init() {
 
@@ -30,11 +70,18 @@ open class Chonks : OpMode() {
         fr = hardwareMap.get(DcMotor::class.java, "FR")
         bl = hardwareMap.get(DcMotor::class.java, "BL")
         br = hardwareMap.get(DcMotor::class.java, "BR")
+        arm = hardwareMap.get(DcMotor::class.java, "Arm" )
+        duckSpinner = hardwareMap.get(DcMotor::class.java, "DuckL" )
+        hi = hardwareMap.get(DcMotor::class.java, "Intake" )
 
         fl.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
         fr.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
         bl.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
         br.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        arm.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        duckSpinner.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        hi.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+
 
         fl.direction = DcMotorSimple.Direction.REVERSE
         bl.direction = DcMotorSimple.Direction.REVERSE
@@ -45,6 +92,9 @@ open class Chonks : OpMode() {
     }
     override fun loop() {
         driveControl()
+        armControl()
+        duckControl()
+        hiControl()
     }
 
 
